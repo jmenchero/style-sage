8dbf29f845812554a2d5f6e9f2cbd85593fa07d5
Juan Menchero <juanbmamigo@gmail.com>
2021-06-20 19:05:42 +0200

    feature: project selector

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,26 +1,16 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
+    <nav class="navbar header has-shadow">
       <div class="navbar-brand">
         <a class="navbar-item" href="/">
-          <img src="~assets/buefy.png" alt="Buefy" height="28" />
+          <img src="~assets/logo.svg" class="logo" />
         </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
       </div>
     </nav>
 
     <section class="main-content columns">
       <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">General</p>
+        <p class="menu-label is-hidden-touch">Projects</p>
         <ul class="menu-list">
           <li v-for="(item, key) of items" :key="key">
             <nuxt-link :to="item.to" exact-active-class="is-active">
@@ -43,17 +33,28 @@ export default {
     return {
       items: [
         {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' },
+          title: 'Cat-Clicker',
+          icon: 'cat',
+          to: { name: 'cats' },
         },
         {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' },
+          title: 'Gifs',
+          icon: 'file-code',
+          to: { name: 'gifs' },
+        },
+        {
+          title: 'Coupons',
+          icon: 'chart-bar',
+          to: { name: 'coupons' },
         },
       ],
     }
   },
 }
 </script>
+
+<style scoped>
+.logo {
+  height: 100%;
+}
+</style>
